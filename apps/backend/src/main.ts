febda8907046fc440677043a260e2e0fc76e924c
fretzestavillo <estavillofretz@gmail.com>
2024-11-27 00:24:19 +0800

    test

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,17 +8,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const HOST = '0.0.0.0';
 const PORT = Number(process.env.PORT) || 8080;
-
+const globalPrefix = 'api';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 8080;
-  await app.listen(PORT, HOST);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${PORT}/${globalPrefix}`,
-  );
+  await app.listen(PORT, HOST).then(() => {
+    console.log(
+      `🚀 Nest Live Development Server is listening on ${HOST}:${PORT}, open your browser on http://localhost:${PORT}/${globalPrefix}`,
+    );
+  });
 }
 
 bootstrap();
