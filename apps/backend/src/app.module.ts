70e15a378b2e808db4b201d5f070408a2683401f
fretzestavillo <estavillofretz@gmail.com>
2024-11-26 19:21:51 +0800

    test

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -7,12 +7,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      //   host: 'host.docker.internal',
-      host: 'postgres',
-      port: 5432,
-      username: 'journal',
-      password: 'journal',
-      database: 'journal',
+      host: process.env.POSTGRES_INSTANCE_UNIX_SOCKET, // Cloud SQL instance connection
+      port: parseInt(process.env.POSTGRES_DB_PORT, 10),
+      username: process.env.POSTGRES_DB_USER,
+      password: process.env.POSTGRES_DB_PASS,
+      database: process.env.POSTGRES_DB_NAME,
       entities: [JournalEntity],
       synchronize: true,
     }),
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
@@ -8,11 +7,11 @@ export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/web',
   server: {
-    port: 4200,
-    host: 'localhost',
+    port: 8080,
+    host: '0.0.0.0',
   },
   preview: {
-    port: 4300,
+    port: 8080,
     host: 'localhost',
   },
   plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
@@ -29,3 +28,37 @@ export default defineConfig({
     },
   },
 });
+
+// DEVELOPMENT
+
+// /// <reference types='vitest' />
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+// import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+
+// export default defineConfig({
+//   root: __dirname,
+//   cacheDir: '../../node_modules/.vite/apps/web',
+//   server: {
+//     port: 4200,
+//     host: 'localhost',
+//   },
+//   preview: {
+//     port: 4300,
+//     host: 'localhost',
+//   },
+//   plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+//   // Uncomment this if you are using workers.
+//   // worker: {
+//   //  plugins: [ nxViteTsPaths() ],
+//   // },
+//   build: {
+//     outDir: '../../dist/apps/web',
+//     emptyOutDir: true,
+//     reportCompressedSize: true,
+//     commonjsOptions: {
+//       transformMixedEsModules: true,
+//     },
+//   },
+// });
